3d155a6732e7c7a4628ba3297592661bfc490f26
rayanadir <rayan.dahmena@gmail.com>
2022-04-16 20:17:59 +0200

    changement composant Welcome en classe

--- src/containers/welcome/Welcome.jsx
+++ src/containers/welcome/Welcome.jsx
@@ -1,18 +1,36 @@
 import './Welcome.scss'
 import React from 'react'
+import ApiData from '../../models/ApiData';
 
-const Welcome = () => {
-    const name="Thomas";
-    const message="Félicitations ! Vous avez explosé vos objectifs hier 👏";
-    return(
+class Welcome extends React.Component {
+  
+    constructor(props){
+        super(props)
+        this.state={
+            id:this.props.id,
+            data:{},
+            loading:true,
+        } 
+    }
+
+    componentDidMount=()=>{
+        ApiData.getUserMainData(this.state.id).then((res)=> {this.setState({data:res.data,loading:false})});
+    }
+    
+    render(){
+    const message = "Félicitations ! Vous avez explosé vos objectifs hier 👏";
+    return this.state.loading === true ?
+    (<div></div>):
+    (    
         <section className="welcome">
             <div className="welcome__hello_container">
                 <h2 className="welcome__hello">Bonjour</h2>
-                <h2 className="welcome__name">{name}</h2>
+                <h2 className="welcome__name">{this.state.data.userInfos.firstName}</h2>
             </div>
             <p className="welcome__message">{message}</p>
         </section>
     )
+    }
 }
 
 export default Welcome
