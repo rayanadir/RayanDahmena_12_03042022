91be342d1b10c27565d01c27854217523b5c86bd
rayanadir <rayan.dahmena@gmail.com>
2022-04-16 20:16:37 +0200

    changement composant Cards en classe

--- src/containers/cards/Cards.jsx
+++ src/containers/cards/Cards.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import './Cards.scss';
 import Card from "../../components/card/Card";
+import ApiData from "../../models/ApiData";
 
-const Cards = () => {
-    return(
-        <section className="cards">
-            <Card type="Calories" value={2500}/>
-            <Card type="Glucides" value={150} />
-            <Card type="Lipides" value={120} />
-            <Card type="Proteines" value={90} />
-        </section>
-    )
+class Cards extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            id:this.props.id,
+            data:{},
+            loading:true,
+          }
+    }
+    componentDidMount=()=>{
+        ApiData.getUserMainData(this.state.id).then((res)=>{this.setState({data:res.data.keyData,loading:false})})
+    }
+    render(){
+        return this.state.loading=== true ? (
+            <div></div>
+        ):
+        (
+            <section className="cards">
+                <Card type="Calories" value={this.state.data.calorieCount}/>
+                <Card type="Glucides" value={this.state.data.proteinCount} />
+                <Card type="Lipides" value={this.state.data.carbohydrateCount} />
+                <Card type="Proteines" value={this.state.data.lipidCount} />
+            </section>
+        )
+    }  
 }
 
 export default Cards
