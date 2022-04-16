a91404025b0402030623ac663c73620d28cecf4a
rayanadir <rayan.dahmena@gmail.com>
2022-04-16 20:24:39 +0200

    mise à jour Dashboard

---
+++ src/views/dashboard/Dashboard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../dashboard/Dashboard.scss'
+import Welcome from '../../containers/welcome/Welcome';
+import Activity from '../../components/activity/Activity';
+import Charts from '../../containers/charts/Charts';
+import Cards from '../../containers/cards/Cards';
+import { useParams } from 'react-router-dom';
+
+const Dashboard = () => {
+    let {id} = useParams();
+    return (
+        <div className="wrap">
+            <Welcome id={id}/>
+            <section className='activity_charts_cards'>
+                <div className="activity_charts">
+                    <Activity id={id} />
+                    <Charts id={id}/>
+                </div>
+                <Cards id={id}/>
+            </section>
+        </div>
+    )
+}
+
+export default Dashboard
