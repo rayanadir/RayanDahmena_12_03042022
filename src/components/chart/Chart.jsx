8276fb74dd94f33abbfcb01f0b1fd5ca1e1e20e4
rayanadir <rayan.dahmena@gmail.com>
2022-04-17 20:15:34 +0200

    changement récupération données composant Chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -8,25 +8,43 @@ class Chart extends React.Component {
         super(props);
         this.state={
             id:this.props.id,
-            data:{},
             loading:true,
             type:this.props.type,
             value:this.props.value
         }
     }
 
-    getValue=()=>{
+    getData=()=>{
         if(this.state.type==="performance"){
-            return ApiFormat.formatUserPerformance(this.state.value);
+            let performance=[];
+            const data=this.state.value.data;
+            const kind=this.state.value.kind;
+            data.map((element,index)=>{
+                const kind_value = kind[index+1].charAt(0).toUpperCase() + kind[index+1].slice(1);
+                const value=element.value
+                const result={value,kind_value};
+                return performance.push(result);
+            })
+            return performance;
         }else if(this.state.type==="average_sessions"){
-
+            let data=this.state.value;
+            const days=['L','M','M','J','V','S','D'];
+            let sessions=[];
+            data.map((session)=>{
+                return sessions.push({
+                    day:days[session.day-1],
+                    sessionLength:session.sessionLength
+                })
+            })
+            return sessions;
         }else if(this.state.type==="score"){
-
+            return this.state.value;
         }
     }
     
     render(){
-        const value=this.getValue();
+       const value= this.getData();
+       console.log(value);
         return (
             <div></div>
         )
