9b87c0ef5825b16ef7b6b7b9ea66dd7658ca8992
rayanadir <rayan.dahmena@gmail.com>
2022-04-24 16:37:28 +0200

    adding corrections and commentaries

--- src/containers/charts/Charts.jsx
+++ src/containers/charts/Charts.jsx
@@ -4,50 +4,78 @@ import Chart from '../../components/chart/Chart';
 import ApiData from '../../models/ApiData';
 import PropTypes from 'prop-types';
 
+/**
+ * @class
+ * @classdesc Charts class component, renders a group of charts
+ */
 class Charts extends React.Component {
-    constructor(props){
+    /**
+     * Get props and declares states
+     * @param { String } props 
+     */
+    constructor(props) {
         super(props);
-        this.state={
-            id:this.props.id,
-            sessions:{},
-            performance:{},
-            score:{},
-            loading_sessions:true,
-            loading_performance:true,
-            loading_score:true,
+        this.state = {
+            id: this.props.id,
+            sessions: {},
+            performance: {},
+            score: {},
+            loading_sessions: true,
+            loading_performance: true,
+            loading_score: true,
         }
     }
+
     componentDidMount = () => {
         ApiData.getUserAverageSessions(this.state.id)
-        .then((res)=>{this.setState(
-            {sessions:res.data.sessions,
-            loading_sessions:false})});
+            .then((res) => {
+                this.setState(
+                    {
+                        sessions: res.sessions,
+                        loading_sessions: false
+                    })
+            });
+
         ApiData.getUserPerformance(this.state.id)
-        .then((res)=>{this.setState(
-            {performance:res.data,
-            loading_performance:false})});
+            .then((res) => {
+                this.setState(
+                    {
+                        performance: Object.values(res),
+                        loading_performance: false
+                    })
+            });
+
         ApiData.getUserMainData(this.state.id)
-        .then((res)=>{this.setState(
-            {score:res.data.score || res.data.todayScore,
-            loading_score:false})});
+            .then((res) => {
+                this.setState(
+                    {
+                        score: res.score,
+                        loading_score: false
+                    })
+            });
     }
-    render(){
-        return this.state.loading_sessions === false && this.state.loading_performance===false && this.state.loading_score===false ? 
-        (
-            <div className="charts">
-                <Chart type="average_sessions" value={this.state.sessions}/>
-                <Chart type="performance" value={this.state.performance} />
-                <Chart type="score" value={this.state.score} />
-            </div>
-        )
-        :(
-            <div></div>
-        )
+
+    /**
+     * Renders a group of charts
+     * @returns { HTMLElement }
+     */
+    render() {
+        return this.state.loading_sessions === false && this.state.loading_performance === false && this.state.loading_score === false ?
+            (
+                <div className="charts">
+                    <Chart type="average_sessions" value={this.state.sessions} />
+                    <Chart type="performance" value={this.state.performance} />
+                    <Chart type="score" value={this.state.score} />
+                </div>
+            )
+            : (
+                <div></div>
+            )
     }
 }
 
-Charts.propTypes={
-    id:PropTypes.string
+Charts.propTypes = {
+    id: PropTypes.string
 }
 
 export default Charts
