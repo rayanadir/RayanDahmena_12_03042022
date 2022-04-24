8e00039ca1fc15b1e5e2c9ad98c54a8857372b9b
rayanadir <rayan.dahmena@gmail.com>
2022-04-24 16:37:52 +0200

    adding commentaries

--- src/containers/welcome/Welcome.jsx
+++ src/containers/welcome/Welcome.jsx
@@ -3,39 +3,52 @@ import React from 'react'
 import ApiData from '../../models/ApiData';
 import PropTypes from 'prop-types';
 
+/**
+ * @class@classdesc Welcome class component, renders a welcome message
+ */
 class Welcome extends React.Component {
-  
-    constructor(props){
+    /**
+     * Get props and declares states
+     * @param { String } props 
+     */
+    constructor(props) {
         super(props)
-        this.state={
-            id:this.props.id,
-            data:{},
-            loading:true,
-        } 
+        this.state = {
+            id: this.props.id,
+            data: {},
+            loading: true,
+        }
     }
 
-    componentDidMount=()=>{
-        ApiData.getUserMainData(this.state.id).then((res)=> {this.setState({data:res.data,loading:false})});
+    componentDidMount = () => {
+        ApiData.getUserMainData(this.state.id)
+        .then((res) => {
+            this.setState({ data: res, loading: false })
+        });
     }
-    
-    render(){
-    const message = "Félicitations ! Vous avez explosé vos objectifs hier 👏";
-    return this.state.loading === true ?
-    (<div></div>):
-    (    
-        <section className="welcome">
-            <div className="welcome__hello_container">
-                <h2 className="welcome__hello">Bonjour</h2>
-                <h2 className="welcome__name">{this.state.data.userInfos.firstName}</h2>
-            </div>
-            <p className="welcome__message">{message}</p>
-        </section>
-    )
+
+    /**
+     * Renders a welcome message
+     * @returns { HTMLElement }
+     */
+    render() {
+        const message = "Félicitations ! Vous avez explosé vos objectifs hier 👏";
+        return this.state.loading === true ?
+            (<div></div>) :
+            (
+                <section className="welcome">
+                    <div className="welcome__hello_container">
+                        <h2 className="welcome__hello">Bonjour</h2>
+                        <h2 className="welcome__name">{this.state.data.userInfos.firstName}</h2>
+                    </div>
+                    <p className="welcome__message">{message}</p>
+                </section>
+            )
     }
 }
 
-Welcome.propTypes={
-    id:PropTypes.string,
+Welcome.propTypes = {
+    id: PropTypes.string,
 }
 
 export default Welcome
