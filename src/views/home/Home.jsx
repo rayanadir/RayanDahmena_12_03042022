fa32ce9c5e1bd4bf5f9580a5f72e1cd91ffde4ed
rayanadir <rayan.dahmena@gmail.com>
2022-04-25 10:23:45 +0200

    add style home

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
     return (
         <section className='home'>
             <Link to="/user/12" className='home__link'>
-                <button className='home__button'>12</button>
+                <button className='home__button'>user 12</button>
             </Link>
             <Link to="/user/18" className='home__link'>
-                <button className='home__button'>18</button>
+                <button className='home__button'>user 18</button>
             </Link>
         </section>
     )
